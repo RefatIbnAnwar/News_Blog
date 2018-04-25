5c5c99f672f1d55effb8cb5dfd1fb5d594d6b338
MySoftheaven BD <mysoftheavenbd@mysoftheavens-mac-mini.local>
2018-04-25 17:37:25 +0600

    Initial Commit

---
+++ News Blog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News Blog
+//
+//  Created by MySoftheaven BD on 25/4/18.
+//  Copyright © 2018 MySoftheaven BD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
